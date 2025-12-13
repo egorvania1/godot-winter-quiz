4feb9632cf948cc44e8cd59707165ac8b8fe94a8
egorvania1 <egorvania1@gmail.com>
2025-12-14 00:14:04 +0500

    Cleaned json to question code. Now sound questions work.

--- scripts/question_chooser.gd
+++ scripts/question_chooser.gd
@@ -16,34 +16,31 @@ func _ready() -> void:
 func _on_question_pressed(questionName: String) -> void:
 	var questionData : Dictionary = questionsData[questionName]
 	var questionType = questionData["type"]
+	
+	questionGrid.visible = false
+	var newQuestion = pictureScene.instantiate()
+	newQuestion.questionType = questionData["type"]
+	newQuestion.questionName = questionData["name"]
+	newQuestion.questionDifficulty = questionData["difficulty"]
+	newQuestion.answerPicPath = questionData["answerPicPath"]
+	newQuestion.answer = questionData["answer"]
+	
 	match questionType:
 		"sound":
-			print("WIPsound")
+			newQuestion.soundPath = questionData["soundPath"]
 		"picture":
-			questionGrid.visible = false
-			var newQuestion = pictureScene.instantiate()
-			newQuestion.questionType = questionData["type"]
-			newQuestion.questionName = questionData["name"]
-			newQuestion.questionDifficulty = questionData["difficulty"]
-			newQuestion.answerPicPath = questionData["answerPicPath"]
-			newQuestion.answer = questionData["answer"]
 			newQuestion.picturePath = questionData["picturePath"]
-			add_child(newQuestion)
 		"text":
-			questionGrid.visible = false
-			var newQuestion = pictureScene.instantiate()
-			newQuestion.questionType = questionData["type"]
-			newQuestion.questionName = questionData["name"]
-			newQuestion.questionDifficulty = questionData["difficulty"]
-			newQuestion.answerPicPath = questionData["answerPicPath"]
-			newQuestion.answer = questionData["answer"]
 			newQuestion.quote = questionData["quote"]
-			add_child(newQuestion)
+	
+	add_child(newQuestion)
+
 
 func load_json_file(path: String) -> Dictionary:
 	var file = FileAccess.open(path, FileAccess.READ)
 	var parsedFile = JSON.parse_string(file.get_as_text())
 	return parsedFile
-	
+
+
 func showQuestions():
 	questionGrid.visible = true
